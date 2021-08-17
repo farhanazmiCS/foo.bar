bff232253fc22ec547b86ff7e9dcba69e44c1125
Nik Mohammad Farhan bin Azmi <68798786+farhanazmiCS@users.noreply.github.com>
2021-08-17 10:20:03 +0800

    Added solutioniter.py
    Added solutioniter.py, an iterative method of solving this problem.

---
+++ solutioniter.py
@@ -0,0 +1,15 @@
+# Solution "Skipping Work (Iterative)""
+def solution(list1, list2):
+    list1.sort()
+    list2.sort()
+    
+    def checkextra(sorted1, sorted2):
+        if len(sorted1) > len(sorted2):
+            for i in range(len(sorted1)):
+                if sorted1[i] != sorted2[i]:
+                    return sorted1[i]
+        elif len(sorted2) > len(sorted1):
+            for i in range(len(sorted1)):
+                if sorted2[i] != sorted1[i]:
+                    return sorted2[i]
+    return checkextra(list1, list2)
